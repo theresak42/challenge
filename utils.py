ac5c9b2b74e660e3f9b19805ba33918d5640118d
pandahd03 <jakob.seibezeder@gmx.at>
2025-04-29 12:24:35 +0200

    add plotly visualization

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import torch
 import numpy as np
+import librosa
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 def pad(x, length):
         pad_width = ((0, 0), (0, length - x.shape[1]))
@@ -16,3 +19,22 @@ def collate_fn(batch):
     labels = torch.tensor(labels).float()
 
     return specs, labels
+
+
+def pred_to_time(pred, fps):
+      test = librosa.frames_to_time
+
+
+def vis(pred, truths):
+    for p, tru in zip(pred, truths):
+        #print(sum(tru))
+        x = np.arange(len(p))
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=x, y=np.maximum(0,p), mode='lines'))
+        for j, t in enumerate(tru):
+            if t != 0:
+                fig.add_vline(x=j,line=dict(color='rgba(255, 0, 0, 0.5)'))
+        fig.show()
+
+        input()
+    
